1af6895e451ac4773278d909595410f98322998b
KedarG7 <gurav.kedar2005@gmail.com>
2026-07-25 16:23:44 +0530

    test

--- src/Pages/Homepage/Homepage2.jsx
+++ src/Pages/Homepage/Homepage2.jsx
@@ -9,7 +9,7 @@ const Homepage2 = () => {
 export function Function1() {
     return (
         <>
-            <h2 className="text-4xl text-center">Your No is 33310</h2>
+            {/* <h2 className="text-4xl text-center">Your No is 33310</h2> */}
         </>
     )
 }
