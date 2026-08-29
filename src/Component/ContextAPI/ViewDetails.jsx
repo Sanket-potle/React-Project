042b536afa62906a14c81f8e12fc3ffb52aeced5
sanket-potle <saketpotle2429@gmail.com>
2026-08-29 18:06:57 +0530

    Rivision Api Calling

--- src/Component/ContextAPI/ViewDetails.jsx
+++ src/Component/ContextAPI/ViewDetails.jsx
@@ -1,19 +1,19 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 const ViewDetails = () => {
   const [product, setProduct] = useState({});
-  const { id } = useParams()
+  const { id } = useParams();
 
-  console.log('id12222222', id)
+  console.log("id12222222", id);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `https://dummyjson.com/products/${id}`
+          `https://dummyjson.com/products/${id}`,
         );
-        console.log('response:', response.data)
+        console.log("response:", response.data);
         setProduct(response.data);
       } catch (error) {
         console.error("Error fetching product:", error);
@@ -21,12 +21,8 @@ const ViewDetails = () => {
     };
 
     fetchData();
-  }, []);
-  return (
-    <div>
-      {/* {product.price} */}
-    </div>
-  )
-}
+  }, [id]);
+  return <div>{product.price}</div>;
+};
 
 export default ViewDetails;
--- src/Notes.jsx
+++ src/Notes.jsx
@@ -1,6 +1,8 @@
 // Use of NavLink in React?
 // >Use Nav Link for active navigation in your React application. The NavLink component from the 'react-router-dom' library allows you to create navigation links that can automatically apply an "active" class when the link's route matches the current URL. Here's an example of how to use NavLink in your Notes.jsx file:
 
+import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
+
 // 1.what is state?
 // >Current data of the component.
 
@@ -33,5 +35,26 @@
 
 // 11.Custom Hook?
 // >custom hook is the js function which use to remove the duplicate logic. and start with use.
+
+// 12.How to use Router?
+// >a.install npm i rect-router-dom
+// b.main.jsx >
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// ;
+// c.App.jsx
+{
+  /* <Routes>
+  <Route path='/' element={} />
+</Routes> */
+}
+// React Router is a library used to navigate between different pages or components in a React application without reloading the entire page.
+
+// 13.How to make something global in React?
+// >If we want to make something available globally across the application, we can use Context API or a state management library like Redux Toolkit.
+
+// 14.Use of useParams?
+// >Get the dynamic value from the URL.
 
 // Make it App Global
