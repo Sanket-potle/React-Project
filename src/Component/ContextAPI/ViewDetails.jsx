00a72bbf59b757dc7a83dc142eef0c8d25210e54
Sanket-potle <sanketpotle2429@gmail.com>
2026-09-08 18:43:21 +0530

    Contect API

--- src/Component/ContextAPI/ViewDetails.jsx
+++ src/Component/ContextAPI/ViewDetails.jsx
@@ -3,26 +3,26 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const ViewDetails = () => {
-  const [product, setProduct] = useState({});
-  const { id } = useParams();
+    const [product, setProduct] = useState({});
+    const { id } = useParams();
 
-  console.log("id12222222", id);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `https://dummyjson.com/products/${id}`,
-        );
-        console.log("response:", response.data);
-        setProduct(response.data);
-      } catch (error) {
-        console.error("Error fetching product:", error);
-      }
-    };
+    console.log("id12222222", id);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    `https://dummyjson.com/products/${id}`,
+                );
+                console.log("response:", response.data);
+                setProduct(response.data);
+            } catch (error) {
+                console.error("Error fetching product:", error);
+            }
+        };
 
-    fetchData();
-  }, [id]);
-  return <div>{product.price}</div>;
+        fetchData();
+    }, [id]);
+    return <div>{product.price}</div>;
 };
 
 export default ViewDetails;
